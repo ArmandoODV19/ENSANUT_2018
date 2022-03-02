3e4f4d39615ee026efc0d9a813504e5b1b40f85b
armandios19 <fooseaov@gmail.com>
2022-03-02 17:28:31 -0600

    agregando funciones

--- scripts/funciones_plots.R
+++ scripts/funciones_plots.R
@@ -43,9 +43,47 @@ daily_intake_state <- function(x = ensanut_limpia, state, x_name = "Frecuencia a
 
 
 ensanut_limpia %>%
-  filter(alimentos == "uso_barro",
+  filter(alimentos == "bebidas",
          estado == "Aguascalientes") %>%
   select(estado, frec_semana)  %>%
   count()
+
+
+# funcion para plot 3
+# este plot permite evaluar frecuencia de consumo de botanas a la semana a nivel nacional
+# las botanas se pueden modificar por cualquier elemento de la columna de alimentos
+
+week_intake_food <- function(x = ensanut_limpia, food, x_name = "frecuencia a la semana",
+                             y_name = "", title_name, title_alig = 0.5){
+  x %>%
+    filter(alimentos == food) %>%
+    ggplot(aes(x = as.factor(frec_semana), fill = as.factor(frec_semana)))+
+    geom_bar()+
+    xlab(x_name)+
+    ylab(y_name)+
+    ggtitle(title_name)+
+    theme(plot.title = element_text(hjust = title_alig))+
+    theme(legend.position="none")
+}
+
+
+# funcion para plot 4
+# plot de frecuencia de consumo de botanas a la semana por estado
+# se puede modificar el estado y el alimento
+
+week_intake_food_n_state <- function(x = ensanut_limpia, food, state,
+                                     x_name = "Frecuencia a la semana",
+                                     y_name = "", title_name, title_alig = 0.5){
+  x %>%
+    filter(alimentos == food,
+           estado == state) %>%
+    ggplot(aes(x = as.factor(frec_semana), fill = as.factor(frec_semana)))+
+    geom_bar()+
+    xlab(x_name)+
+    ylab(y_name)+
+    ggtitle(title_name)+
+    theme(plot.title = element_text(hjust = title_alig))+
+    theme(legend.position="none")
+}
 
 
