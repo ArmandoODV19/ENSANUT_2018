6c78f146625444ac497ac95445271318760629e7
armandios19 <fooseaov@gmail.com>
2022-03-02 23:54:15 -0600

    region intake function

--- scripts/funciones_plots.R
+++ scripts/funciones_plots.R
@@ -175,3 +175,34 @@ country_intake <- function(x = ensanut_limpia, title_name, title_alig = 0.5,
 }
 
 
+# funcion para plot 20
+# consumo de alimentos filtrado por dominio, sexo, edad_categorica
+
+region_intake <- function(x = ensanut_limpia, zone, domain, sex, age,
+                          title_name, title_alig = 0.5,
+                          x_name = "alimentos",
+                          y_name = "frecuencia absoluta acumulada"){
+  x %>%
+    select(alimentos, estado, dominio, sexo, edad_categorica, frec_semana, region) %>%
+    filter(region == zone,
+           dominio == domain,
+           sexo == sex,
+           edad_categorica == age) %>%
+    group_by(alimentos, estado) %>%
+    summarise_at(vars(frec_semana),
+                 list(name = sum)) %>%
+    ggplot(aes(x=alimentos, y=name, fill = alimentos))+
+    geom_bar(stat = "identity")+
+    facet_wrap(.~estado)+
+    theme(axis.text.x = element_text(size = 10, angle = 90, hjust = 1),
+          axis.text.y = element_text(size = 10),
+          panel.border=element_blank(),
+          strip.background=element_rect(colour="white", fill="white"))+
+    ggtitle(title_name)+
+    theme(plot.title = element_text(hjust = title_alig))+
+    theme(legend.position="none")+
+    xlab(x_name)+
+    ylab(y_name)
+}
+
+
